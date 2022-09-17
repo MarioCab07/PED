a11568f6648080649fdd99d7d569d2ee7bebab33
Mario Cabrera <luismct678@gmail.com>
2022-09-17 11:21:22 -0600

    ignore

--- cp4pe.cpp
+++ cp4pe.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 void compArreglo(int *x, int y);
+
+void ordenar(int *x, int y);
+
+void imprimir(int *x, int y);
+
+
 int main(){
 
    int *p, n;
@@ -11,8 +17,12 @@ int main(){
    p= new int [n];
 
    compArreglo(p,n);
-   delete[] p;
+   
+   //imprimir(p,n);
 
+   ordenar(p,n);
+   imprimir(p,n);
+   delete[] p;
 
    return 0;
 }
@@ -22,5 +32,39 @@ void compArreglo(int *x, int y){
    for(int i=0; i<y; i++){
 
       cout<<"Ingrese el elemento "<<i+1<<": "; cin>>x[i];
+   }
+}
+
+void ordenar(int *x, int y){
+
+   int max, aux, i,j;
+
+   for(i=0; i<y;i++){
+
+      max=i;
+
+      for(j=i+1; j<y;j++){
+
+         if(x[j]>x[max]){
+            max=j;
+         }
+      }
+
+      aux=x[i];
+      x[i]=x[max];
+      x[max]=aux;
+   }
+
+
+
+}
+
+
+void imprimir(int *x, int y){
+
+   for (int i=0; i<y; i++){
+
+      cout<<"Elemento "<<i+1<<": "<<x[i]<<endl<<endl;
+      
    }
 }
