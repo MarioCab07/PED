1698b51e1b5ca90169a83ee5ff9d96d7156929de
Mario Cabrera <luismct678@gmail.com>
2022-09-17 12:55:44 -0600

    bucle

--- cp4pe.cpp
+++ cp4pe.cpp
@@ -13,19 +13,21 @@ struct Paquetes
    int peso;
    string Fragil;
    char Tamanio;
-} paquete[5];
+} paquete[3];
 
-void completar();
+void completar(int);
 
 int main()
 {
-   int num, intento = 0, n;
+   int num,n,  repetir;
    
+   n=sizeof(paquete)/sizeof(paquete[0]);
    
-   completar();
-   cout<<"Ingrese el ID de un paquete: "; cin>>num;
-do{
-   for( int i=0; i<5;i++){
+   completar(n);
+   
+   cout<<"Ingrese un ID: "; cin>>num;
+   
+   for( int i=0; i<n;i++){
       
       if(paquete[i].ID==num){
          cout<<"ID: "<<paquete[i].ID<<endl;
@@ -33,31 +35,31 @@ do{
          cout << "Fragil: " << paquete[i].Fragil << endl;
          cout << "Tamanio: " << paquete[i].Tamanio << endl;
       }else{
-         intento++;
+         cout<<"El ID ingresado no corresponde con ningun paquete"<<endl;
         
       }
+         
 
-      if(intento>3){
-         break;
-      }
-   }}while(intento<=3);
+         //cout<<"Desea buscar otro paquete? Si=1 - No=2"<<endl; cin>>repetir;
+      
+   
 
 
 return 0;
 }
 
-void completar(){
+void completar(int n){
 
-   for(int i=0; i<5;i++){
-      srand(time(NULL));
-      paquete[i].ID= 1 + rand() % (100);
-      cout<<"ID del paquete "<<i+1<<": "<<paquete[i].ID<<endl;
-      cout<<"Ingrese el peso en kg del paquete "<<i+1<<": "; cin>>paquete[i].peso;
-      cout << "El paquete " << i + 1 << " es fragil? ";cin >> paquete[i].Fragil;  
-      cout<<"Ingrese el tamanio del paquete (P= pequenio, M=mediano, G=grande): "; cin>>paquete[i].Tamanio;
-
-   }
-   
-
-   
+   srand(time(NULL));
+   paquete[i].ID = 1 + rand() % (100);
+   cout << "ID del paquete " << i + 1 << ": " << paquete[i].ID << endl;
+   cout << "Ingrese el peso en kg del paquete " << i + 1 << ": ";
+   cin >> paquete[i].peso;
+   cout << "El paquete " << i + 1 << " es fragil? ";
+   cin >> paquete[i].Fragil;
+   cout << "Ingrese el tamanio del paquete (P= pequenio, M=mediano, G=grande): ";
+   cin >> paquete[i].Tamanio;
 }
+
+
+
