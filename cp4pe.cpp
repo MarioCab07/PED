394fbaa5860f241c3ce532048e7c94b66ccee96d
Mario Cabrera <luismct678@gmail.com>
2022-09-17 10:45:52 -0600

    nuevo

--- cp4pe.cpp
+++ cp4pe.cpp
@@ -2,11 +2,25 @@
 
 using namespace std;
 
+void compArreglo(int *x, int y);
 int main(){
 
    int *p, n;
+   cout<<"Ingrese el numero de elementos: "; cin>>n;
 
    p= new int [n];
 
+   compArreglo(p,n);
+   delete[] p;
+
+
    return 0;
+}
+
+void compArreglo(int *x, int y){
+
+   for(int i=0; i<y; i++){
+
+      cout<<"Ingrese el elemento "<<i+1<<": "; cin>>x[i];
+   }
 }
